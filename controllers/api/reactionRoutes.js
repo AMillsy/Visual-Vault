cd07659e60f608defc8c76099a74461f40ec6989
poisoned-eden <poisoned.eden@gmail.com>
2023-07-13 21:29:34 +0100

    adds api routes for post get and delete reactions

---
+++ controllers/api/reactionRoutes.js
@@ -0,0 +1,56 @@
+const { Reaction } = require('../../models');
+const router = require('express').Router();
+
+router.post(`/`, async (req, res) => {
+    const { type, user_id, project_id } = req.body
+    try {
+		const newReaction = await Reaction.create({
+			type: type,
+			user_id: req.session.user_id,
+            project_id: project_id,
+		});
+
+		res.status(200).json(newReaction);
+	} catch (err) {
+		res.status(400).json(err);
+	}  
+});
+
+
+router.get(`/`, async (req, res) => {
+    try {
+        const reactionData = await Reaction.findAll({
+            where: { project_id: id },
+        });
+
+        if (!reactionData) {
+			res.status(404).json({ message: 'No reactions for this project id' });
+			return;
+		}
+
+		res.status(200).json(reactionData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/', async (req, res) => {
+    try {
+        const reactionData = await Reaction.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!reactionData) {
+			res.status(404).json({ message: 'No reaction found with this id!' });
+			return;
+		}
+
+		res.status(200).json(reactionData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
