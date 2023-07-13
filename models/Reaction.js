9740177e9fc26633e3b6daa98bf3cf672523cf12
poisoned-eden <poisoned.eden@gmail.com>
2023-07-13 20:31:57 +0100

    removes unnecessary attributes in models

--- models/Reaction.js
+++ models/Reaction.js
@@ -5,20 +5,9 @@ class Reaction extends Model {}
 
 Reaction.init(
 	{
-		id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true,
-		},
 		type: {
 			type: DataTypes.STRING,
 			allowNull: false,
-		},
-		date_created: {
-			type: DataTypes.DATE,
-			allowNull: false,
-			defaultValue: DataTypes.NOW,
 		},
 		user_id: {
 			type: DataTypes.INTEGER,
@@ -37,8 +26,6 @@ Reaction.init(
 	},
 	{
 		sequelize,
-		timestamps: false,
-		freezeTableName: true,
 		underscored: true,
 		modelName: 'reaction',
 	}
