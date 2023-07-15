207853c562923d24265d03bc7dfddb7ef7a53664
AMillsy <00adammills00@gmail.com>
2023-07-15 18:24:47 +0100

    removing logs on project and homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -60,7 +60,10 @@ router.get('/project/:id', async (req, res) => {
 
 		const project = projectData.get({ plain: true });
 
-		res.render('project', { project: project });
+		res.render('project', {
+			project: project,
+			logged_in: req.session.logged_in,
+		});
 	} catch (error) {
 		res.status(500).json({ message: `AN ERROR HAS OCCURRED` });
 	}
@@ -80,7 +83,7 @@ router.get('/profile', withAuth, async (req, res) => {
 		});
 
 		const user = userData.get({ plain: true });
-
+		console.log(user);
 		res.render('profile', {
 			...user,
 			logged_in: req.session.logged_in,
