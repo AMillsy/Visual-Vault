016d4afd5bf590910b52e13181c99e998bb4cc0f
AMillsy <00adammills00@gmail.com>
2023-07-17 20:51:09 +0100

    fix to the recent routes

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 const { uploadProjects, uploadUsers, s3 } = require(`./s3connection`);
+const { sequelize } = require('./connection');
 
-module.exports = { uploadProjects, uploadUsers, s3 };
+module.exports = { uploadProjects, uploadUsers, s3, sequelize };
