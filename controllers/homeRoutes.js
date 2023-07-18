9bddded4094ca359e16f40be2b41a6cdbd45d85f
AMillsy <00adammills00@gmail.com>
2023-07-18 19:53:39 +0100

    socials in the profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -91,7 +91,7 @@ router.get('/profile', withAuth, async (req, res) => {
 		});
 
 		const user = userData.get({ plain: true });
-		console.log(req.session.profile);
+		console.log(user);
 		res.render('profile', {
 			...user,
 			logged_in: req.session.logged_in,
