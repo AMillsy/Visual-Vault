d08cbe347006181c3357161ba6fbe17ef6975d25
poisoned-eden <poisoned.eden@gmail.com>
2023-07-19 10:46:42 +0100

    changes numbers when clicked

--- public/js/reactions_interactivity.js
+++ public/js/reactions_interactivity.js
@@ -1,85 +1,78 @@
-const arrayReactionButtonEls = document
+const reactionButtonEls = document
 .querySelectorAll('button.reaction-button');
 
-const reactionHandler = async (event) => {
-	event.preventDefault();
+reactionButtonEls.forEach( (button) => {
+    button.addEventListener('click', async function () {
+        project_id = this.closest('.reaction-buttons').getAttribute('data-id');
+        var type = "";
+        var numberReacts = parseInt(this.innerText);
 
-    const clickedButtonEl = event.srcElement;
-    var number = parseInt(clickedButtonEl.innerText);
-    const project_id = clickedButtonEl.closest('.reaction-buttons').getAttribute('data-id');
-    var type = "";
-    
-    var listOfClasses = clickedButtonEl.classList;
-    // sets type to whats in class
-    switch ( true ) {
-        case listOfClasses.contains('thumbs_up'):
-            type = "thumbs-up";
-            break;
-
-        case listOfClasses.contains('stars'):
-            type = "stars";
-            break;
-
-        case listOfClasses.contains('heart'):
-            type = "heart";
-            break;
-
-        case listOfClasses.contains('bullseye'):
-            type = "bullseye";
-            break;        
-
-        default:
-            alert('not a reaction button that was clicked... not sure how you saw this alert...');
-            break;
-    };
-    
-    if (clickedButtonEl.classList.contains('active-reaction')) {
-        // remove class that makes it appear active
-        clickedButtonEl.classList.remove('active-reaction');
-        number -= 1;
-        clickedButtonEl.innerText(number);
-
-        // remove reaction from db
-        const response = await fetch('/api/reactions', {
-            method: 'DELETE',
-            body: JSON.stringify({ 
-                type: type,
-                project_id: project_id
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-    
-        if (response.ok) {
-                console.log(response);
-        } else {
-            alert(response.statusText);
+        var listOfClasses = this.classList;
+        // sets type to whats in class
+        switch ( true ) {
+            case listOfClasses.contains('thumbs_up'):
+                type = "thumbs-up";
+                break;
+        
+            case listOfClasses.contains('stars'):
+                type = "stars";
+                break;
+        
+            case listOfClasses.contains('heart'):
+                type = "heart";
+                break;
+        
+            case listOfClasses.contains('bullseye'):
+                type = "bullseye";
+                break;        
+        
+            default:
+                break;
         };
-
-    } else {
-        // add class that makes it appear active
-        clickedButtonEl.classList.add('active-reaction');
-        number += 1;
-        clickedButtonEl.innerText(number);
-
-        // add reaction to db
-        const response = await fetch('/api/reactions', {
-            method: 'POST',
-            body: JSON.stringify({ 
-                type: type,
-                project_id: project_id
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-    
-        if (response.ok) {
-            console.log(response);
+        
+        if (this.classList.contains('active-reaction')) {
+            // remove class that makes it appear active
+            this.classList.remove('active-reaction');
+            numberReacts -= 1;
+            this.innerText = numberReacts;
+        
+            // remove reaction from db
+            const response = await fetch('/api/reactions', {
+                method: 'DELETE',
+                body: JSON.stringify({ 
+                    type: type,
+                    project_id: project_id
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+        
+            if (response.ok) {
+                    console.log(response);
+            } else {
+                alert(response.statusText);
+            };
+        
         } else {
-            alert(response.statusText);
-        };        
-    };
-    
-};
-
-arrayReactionButtonEls.forEach( (button) => {
-        button.addEventListener('click', reactionHandler);
+            // add class that makes it appear active
+            this.classList.add('active-reaction');
+            numberReacts += 1;
+            this.innerText = numberReacts;
+        
+            // add reaction to db
+            const response = await fetch('/api/reactions', {
+                method: 'POST',
+                body: JSON.stringify({ 
+                    type: type,
+                    project_id: project_id
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+        
+            if (response.ok) {
+                console.log(response);
+            } else {
+                alert(response.statusText);
+            };        
+        };
     });
+});
