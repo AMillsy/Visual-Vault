239d1a21d63fa50783ea624675b7769f2e870c44
AMillsy <00adammills00@gmail.com>
2023-07-27 17:52:41 +0100

    removing logs

--- controllers/api/reactionRoutes.js
+++ controllers/api/reactionRoutes.js
@@ -2,62 +2,64 @@ const { Reaction } = require('../../models');
 const router = require('express').Router();
 
 router.post(`/`, async (req, res) => {
-    const { type, project_id } = req.body;
-    try {
+	const { type, project_id } = req.body;
+	try {
 		const newReaction = await Reaction.create({
 			type: type,
 			user_id: req.session.user_id,
-            project_id: project_id,
+			project_id: project_id,
 		});
 
 		res.status(200).json(newReaction);
 	} catch (err) {
 		res.status(400).json(err);
-	}  
+	}
 });
 
-
 router.get(`/`, async (req, res) => {
-    const { project_id } = req.body;
-    try {
-        const reactionData = await Reaction.findAll({
-            where: { 
-                project_id: project_id 
-            },
-        });
+	const { project_id } = req.body;
+	try {
+		const reactionData = await Reaction.findAll({
+			where: {
+				project_id: project_id,
+			},
+		});
 
-        if (!reactionData) {
-			res.status(404).json({ message: 'No reactions for this project id' });
+		if (!reactionData) {
+			res.status(404).json({
+				message: 'No reactions for this project id',
+			});
 			return;
 		}
 
 		res.status(200).json(reactionData);
-        console.log(res);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 router.delete('/', async (req, res) => {
-    const { type, project_id } = req.body;
-    try {
-        const reactionData = await Reaction.destroy({
-            where: {
-                type: type,
-                user_id: req.session.user_id,
-                project_id: project_id,
-            },
-        });
+	const { type, project_id } = req.body;
+	try {
+		const reactionData = await Reaction.destroy({
+			where: {
+				type: type,
+				user_id: req.session.user_id,
+				project_id: project_id,
+			},
+		});
 
-        if (!reactionData) {
-			res.status(404).json({ message: 'No reaction found with this id!' });
+		if (!reactionData) {
+			res.status(404).json({
+				message: 'No reaction found with this id!',
+			});
 			return;
 		}
 
 		res.status(200).json(reactionData);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 module.exports = router;
